a4feba0dc622a09f100dd88523a139e335af7752
Mark Mulder <markmulder@gmail.com>
2017-11-20 10:48:56 +0100

    Use express to serve app

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
     entry: "./src/index.tsx",
     output: {
         filename: "bundle.js",
-        path: __dirname + "/dist"
+        path: __dirname + "/public"
     },
 
     // Enable sourcemaps for debugging webpack's output.
@@ -21,14 +21,5 @@ module.exports = {
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
-    },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
     },
 };
