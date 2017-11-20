e63deee407ad485dc7aaf55ccd8688580a0ac344
Mark Mulder <markmulder@gmail.com>
2017-11-20 14:06:05 +0100

    Compile JS

--- public/bundle.js
+++ public/bundle.js
@@ -18221,27 +18221,60 @@ var __extends = (this && this.__extends) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = __webpack_require__(2);
+var sound_1 = __webpack_require__(28);
 var Soundboard = /** @class */ (function (_super) {
     __extends(Soundboard, _super);
     function Soundboard() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     Soundboard.prototype.render = function () {
-        var _this = this;
         return (React.createElement("div", { className: "columns" },
-            React.createElement("div", { className: "column" },
-                React.createElement("a", { className: "button is-large is-primary", onClick: function (e) { return _this.onClick(e); } },
-                    React.createElement("span", { className: "icon is-large" },
-                        React.createElement("i", { className: "fa fa-music" }))))));
-    };
-    Soundboard.prototype.onClick = function (e) {
-        var audio = document.getElementById("airhorn");
-        audio.currentTime = 0;
-        audio.play();
+            React.createElement(sound_1.default, { audio: "airhorn" }),
+            React.createElement(sound_1.default, { audio: "doitlive" })));
     };
     return Soundboard;
 }(React.Component));
 exports.Soundboard = Soundboard;
+
+
+/***/ }),
+/* 28 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = __webpack_require__(2);
+var Sound = /** @class */ (function (_super) {
+    __extends(Sound, _super);
+    function Sound(props) {
+        return _super.call(this, props) || this;
+    }
+    Sound.prototype.render = function () {
+        var _this = this;
+        return (React.createElement("div", { className: "column" },
+            React.createElement("a", { className: "button is-large is-primary", onClick: function (e) { return _this.onClick(e); } },
+                React.createElement("span", { className: "icon is-large" },
+                    React.createElement("i", { className: "fa fa-music" })))));
+    };
+    Sound.prototype.onClick = function (e) {
+        var audio = document.getElementById(this.props.audio);
+        audio.currentTime = 0;
+        audio.play();
+    };
+    return Sound;
+}(React.Component));
+exports.default = Sound;
 
 
 /***/ })
