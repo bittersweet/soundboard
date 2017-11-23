32f51c2945794645e3c05a6fccb0df77fc9faf82
Mark Mulder <markmulder@gmail.com>
2017-11-23 17:41:21 +0100

    Add huisbaas bob

--- src/components/soundboard.tsx
+++ src/components/soundboard.tsx
@@ -26,6 +26,27 @@ export class Soundboard extends React.Component {
             <div className="columns">
               <Sound audio="mansnothot"></Sound>
             </div>
+            <div className="columns">
+              <Sound audio="bob-geengulden"></Sound>
+              <Sound audio="bob-geentyfus"></Sound>
+              <Sound audio="bob-heh"></Sound>
+              <Sound audio="bob-hersens"></Sound>
+            </div>
+            <div className="columns">
+              <Sound audio="bob-hoedebaashetdoet"></Sound>
+              <Sound audio="bob-laugh"></Sound>
+              <Sound audio="bob-nooitgeengeld"></Sound>
+              <Sound audio="bob-opjemuil"></Sound>
+            </div>
+            <div className="columns">
+              <Sound audio="bob-optyfen"></Sound>
+              <Sound audio="bob-stukinjebroek"></Sound>
+              <Sound audio="bob-tyfushoer"></Sound>
+              <Sound audio="bob-viezetyfuslijer"></Sound>
+            </div>
+            <div className="columns">
+              <Sound audio="bob-vijfeuros"></Sound>
+            </div>
           </div>
           );
   }
