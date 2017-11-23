f0b9b39cd87131b8b951a066f3ae498ae95ef6f7
Mark Mulder <markmulder@gmail.com>
2017-11-23 13:49:16 +0100

    Add man's not hot sounds

--- src/components/soundboard.tsx
+++ src/components/soundboard.tsx
@@ -4,15 +4,28 @@ import Sound from "./sound";
 export class Soundboard extends React.Component {
   public render() {
       return (
-          <div className="columns">
-            <Sound audio="airhorn"></Sound>
-            <Sound audio="doitlive"></Sound>
-            <Sound audio="alwaysbeclosing"></Sound>
-            <Sound audio="badumtss"></Sound>
-            <Sound audio="drumroll"></Sound>
-            <Sound audio="crickets"></Sound>
-            <Sound audio="mariokart"></Sound>
-            <Sound audio="damnson"></Sound>
+        <div>
+            <div className="columns">
+              <Sound audio="airhorn"></Sound>
+              <Sound audio="doitlive"></Sound>
+              <Sound audio="alwaysbeclosing"></Sound>
+              <Sound audio="badumtss"></Sound>
+            </div>
+            <div className="columns">
+              <Sound audio="drumroll"></Sound>
+              <Sound audio="crickets"></Sound>
+              <Sound audio="mariokart"></Sound>
+              <Sound audio="damnson"></Sound>
+            </div>
+            <div className="columns">
+              <Sound audio="quickmaths"></Sound>
+              <Sound audio="noketchup"></Sound>
+              <Sound audio="rawsauce"></Sound>
+              <Sound audio="sbkrap"></Sound>
+            </div>
+            <div className="columns">
+              <Sound audio="mansnothot"></Sound>
+            </div>
           </div>
           );
   }
